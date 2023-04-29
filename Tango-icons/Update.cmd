513a9158602d4f5ca338234b09019858abd86c05
gigaherz <gigaherz@gmail.com>
2023-04-29 10:56:46 +0200

    Import some icons from the public domain Tango icon library. The files font-size-up.svg and font-size-down.svg are made by combining parts of other icons from the set. XAML conversion via the SvgToXaml tool (https://github.com/BerndK/SvgToXaml). Re-run Update.cmd if adding or changing icons.

---
+++ Tango-icons/Update.cmd
@@ -0,0 +1,3 @@
+rem SvgToXaml.exe BuildDict /inputdir "." /outputdir "..\GBookEdit" /outputname TangoIcons /useComponentResKeys=true /compResKeyNSName=GBookEdit /compResKeyNS=GBookEdit
+SvgToXaml.exe BuildDict /inputdir "." /outputdir "..\GBookEdit" /outputname TangoIcons
+pause
